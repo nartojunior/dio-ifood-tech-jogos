17da2168a1a55dd7f038041b49a1c685f99dc994
Narto Junior <nartojunior@outlook.com>
2023-11-29 00:05:03 -0300

    feat(js): add data types initial class

--- js/ex01-variaveis-constante.js
+++ js/ex01-variaveis-constante.js
@@ -3,6 +3,7 @@
 
 /* declarando variáveis */
 
+var ler = true // variavel de escopo global
 let i = 10  // variavel mutável em tempo de execução
 const PI = 3.14 // Constante imutável, necessita ser atribuída na declaração
 
@@ -11,4 +12,30 @@ console.log(i)
 i = 35
 console.log (i)
 console.log(PI)
+
+/* Hoisting (Içamento) */
+/* 
+    O javascript coloca todas as declarações antes das execuções,
+    como se ele colocasse os identificadores no início do arquivo
+    antes de analisar as execuções.
+*/
+
+// É possível executar uma função antes de sua declaração
+
+mostre()
+function mostre() { console.log("mostrando") }
+
+//a() // | Não é possível usar quando a atribuição é de um método/function
+
+let a = function() { console.log("anonima")}
+
+// efeito global de variavel declarar com 'var'
+
+if (true) { var valor = 12 } // variável global declarada no escopo do 'if' (local)
+console.log(valor) // acessando valor da variável global.
+// vazamento de escopo.
+// variável 'var' não sofrem hoisting em função.
+function teste() { var valor2 = 10 }
+teste()
+// console.log(valor2) // Erro de acesso de variável não atribuída.
 
---
+++ js/ex08_tipos_de_dados.js
@@ -0,0 +1,15 @@
+/* Tipos de Dados */
+
+// boolean
+// null
+// undefined
+// Number
+// string
+// symbol
+// Object
+
+let x = { nome: undefined}
+console.log(JSON.stringify(x))
+
+let y = { nome: null}
+console.log(JSON.stringify(y))
